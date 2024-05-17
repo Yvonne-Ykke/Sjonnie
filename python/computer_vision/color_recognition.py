022beaf7deecf8ea75f856c336a7ddffa0a4ae62
Yvonne-Ykke <yvonne.koeroo@student.nhlstenden.com>
2024-05-17 10:23:34 +0200

    made a main module, contour and color recognition both work

--- python/computer_vision/color_recognition.py
+++ python/computer_vision/color_recognition.py
@@ -4,14 +4,18 @@ import cv2 as cv
 developing = True
 
 class Color:
-    def __init__(self, name, hsv_range):
+    def __init__(self, name, low_hsv, high_hsv, low_hsv2 = None, high_hsv2 = None):
         self.name = name
-        self.hsv_range = hsv_range
+        self.low_hsv = low_hsv
+        self.high_hsv = high_hsv
+        self.low_hsv2 = low_hsv2
+        self.high_hsv2 = high_hsv2
 
 colors = [
-    Color("blue", [[90, 45, 45], [135, 255, 255]]),
-    Color("red", [[0, 80, 80], [15, 255, 255], [165, 80, 80], [179, 255, 255]]),
-    Color("green", [[37, 30, 30], [90, 255, 255]]),
+    Color("blue", [90, 45, 45], [135, 255, 255]),
+    Color("red", [0, 80, 80], [15, 255, 255], [165, 80, 80], [179, 255, 255]),
+    Color("green", [37, 30, 30], [90, 255, 255]),
+    Color("yellow", [16, 100, 100], [36, 255, 255]),
     # TODO: Add more colors
 ]
 
@@ -20,27 +24,32 @@ def masks(frame):
     color_masks = []
     for color in colors:
         mask = np.zeros_like(frame)
-        for hsv_range in color.hsv_range:
-            mask = cv.inRange(hsv, np.array(hsv_range[0]), np.array(hsv_range[1]))
-            if len(hsv_range) == 4:
-                mask2 = cv.inRange(hsv, np.array(hsv_range[2]), np.array(hsv_range[3]))
-                mask = mask | mask2
+        mask = cv.inRange(hsv, np.array(color.low_hsv), np.array(color.high_hsv))
+
+        if np.array(color.low_hsv2) is not None and color.high_hsv2 is not None:
+            mask2 = cv.inRange(hsv, np.array(color.low_hsv2), np.array(color.high_hsv2))
+            mask = mask | mask2
                                        
         color_masks.append((color.name.capitalize(), mask))
     return color_masks
 
-cap = cv.VideoCapture(0)
-while(True):
-    ret,img = cap.read()
-    if img is None:
-        break
-    cv.imshow("image", img)
-    color_masks = masks(img)
-    for color_name, mask in color_masks:
-        res = cv.bitwise_and(img,img, mask= mask)
-        cv.imshow(color_name, res)
+def detect(developing):
+    if developing:
+        cap = cv.VideoCapture(0)
+    else:
+        cap = cv.VideoCapture(1)
     
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        cap.release()
-        cv.destroyAllWindows()
-        break
+    while(True):
+        ret,img = cap.read()
+        if img is None:
+            break
+        cv.imshow("image", img)
+        color_masks = masks(img)
+        for color_name, mask in color_masks:
+            res = cv.bitwise_and(img,img, mask= mask)
+            cv.imshow(color_name, res)
+        
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            cv.destroyAllWindows()
+            break
