b3382e8d0862514aeaa8282cf8b1a45b0dead331
JoostBakker28 <113503680+JoostBakker28@users.noreply.github.com>
2024-06-06 12:55:25 +0200

    contrast toegevoegd

--- python/computer_vision/color_recognition.py
+++ python/computer_vision/color_recognition.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2 as cv
 
-developing = False
+developing = True
 
 class Color:
-    def __init__(self, name, low_hsv, high_hsv, bgr = None, low_hsv2 = None, high_hsv2 = None):
+    def __init__(self, name, low_hsv, high_hsv, bgr=None, low_hsv2=None, high_hsv2=None):
         self.name = name
         self.low_hsv = low_hsv
         self.high_hsv = high_hsv
@@ -14,24 +14,32 @@ class Color:
 
 colors = [
     Color("blue", [80, 60, 0], [140, 255, 255], [255, 0, 0]),
-    Color("red", [0, 70, 0], [15, 255, 255], [175, 70, 60], [179, 255, 255], [0, 0, 255]),
+    Color("red", [0, 70, 50], [10, 255, 255], [0, 0, 255], [170, 70, 50], [180, 255, 255]),
     Color("green", [40, 41, 74], [86, 255, 255], [0, 255, 0]),
     Color("yellow", [16, 80, 0], [36, 255, 255], [0, 255, 255]),
-    Color("pink", [167, 63, 100], [176, 255, 255], [0, 50, 255]),
-    Color("silver", [10, 10, 100], [160, 255, 255],[170,169,173])
+    Color("pink", [140, 50, 50], [170, 255, 255], [0, 50, 255]),
+    Color("silver", [0, 0, 75], [180, 50, 255], [170, 169, 173])
 ]
+
+def apply_clahe(frame):
+    lab = cv.cvtColor(frame, cv.COLOR_BGR2Lab)
+    l, a, b = cv.split(lab)
+    
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    cl = clahe.apply(l)
+    
+    limg = cv.merge((cl, a, b))
+    final = cv.cvtColor(limg, cv.COLOR_Lab2BGR)
+    return final
 
 def masks(frame):
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     color_masks = []
     for color in colors:
-        mask = np.zeros_like(frame)
         mask = cv.inRange(hsv, np.array(color.low_hsv), np.array(color.high_hsv))
-
-        if np.array(color.low_hsv2) is not None and color.high_hsv2 is not None:
+        if color.low_hsv2 is not None and color.high_hsv2 is not None:
             mask2 = cv.inRange(hsv, np.array(color.low_hsv2), np.array(color.high_hsv2))
             mask = mask | mask2
-                                       
         color_masks.append((color.name.capitalize(), mask))
     return color_masks
 
@@ -42,14 +50,17 @@ def detect(developing):
         cap = cv.VideoCapture(1)
     
     while(True):
-        ret,img = cap.read()
+        ret, img = cap.read()
         if img is None:
             break
+        
+        img = apply_clahe(img)
+        
         cv.imshow("image", img)
         color_masks = masks(img)
         results = []
         for color_name, mask in color_masks:
-            res = cv.bitwise_and(img,img, mask= mask)
+            res = cv.bitwise_and(img, img, mask=mask)
             results.append(res)
             cv.imshow(color_name, res)
         
@@ -57,8 +68,6 @@ def detect(developing):
             cap.release()
             cv.destroyAllWindows()
             break
-        
 
 if __name__ == "__main__":
-    detect(False)
-
+    detect(developing)
