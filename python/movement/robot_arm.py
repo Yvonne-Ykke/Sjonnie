b4792382e2df7aa6061fa7c4744614bbec1ff3b7
Yvonne-Ykke <yvonne.koeroo@student.nhlstenden.com>
2024-06-10 08:59:47 +0200

    naming conventions

--- python/movement/robot_arm.py
+++ python/movement/robot_arm.py
@@ -5,12 +5,12 @@ import signal
 
 serial_connection = Connection(port="/dev/ttyS0", baudrate=1000000, rpi_gpio=True, timeout=0.5, waiting_time=0.01)
 
-servo_1 = 61
-servo_2 = 3
+SERVO_1 = 61
+SERVO_2 = 3
 
 def move_to_position(shoulder_angle, elbow_angle):
-    serial_connection.goto(servo_1, shoulder_angle, speed = 20, degrees = True)
-    serial_connection.goto(servo_2, elbow_angle, speed = 20, degrees = True)
+    serial_connection.goto(SERVO_1, shoulder_angle, speed = 20, degrees = True)
+    serial_connection.goto(SERVO_2, elbow_angle, speed = 20, degrees = True)
     print(f"Bewegen naar positie: Schouder hoek: {shoulder_angle}, Elleboog hoek: {elbow_angle}")
 
 def start_tcp_server():
