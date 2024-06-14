4ad4d67b5731db7a3377d4d0dbb4233a9db06534
joelsefanja <joelsefanja@gmail.com>
2024-06-14 12:04:12 +0200

    Scan Ids

--- python/controlling/python_servo_controller/scan_ids.py
+++ python/controlling/python_servo_controller/scan_ids.py
@@ -1,17 +1,23 @@
 from pyax12.connection import Connection
+import RPi.GPIO as GPIO
 import time
-# Connect to the serial port
-serial_connection = Connection(port="/dev/ttyS0", baudrate=1000000, rpi_gpio=True,timeout=1, waiting_time=0.1)
+
+# Schakel GPIO-waarschuwingen uit
+GPIO.setwarnings(False)
+
+# Maak verbinding met de seriële poort
+serial_connection = Connection(port="/dev/ttyS0", baudrate=1000000, rpi_gpio=True, timeout=1, waiting_time=0.1)
 
 time.sleep(0.01)
 
-# Ping the dynamixel unit(s)
+# Ping de Dynamixel eenheden
 ids_available = serial_connection.scan()
 
 time.sleep(0.01)
+
+# Print de gevonden IDs
 for dynamixel_id in ids_available:
     print(dynamixel_id)
 
-print (ids_available)
-# Close the serial connection
+# Sluit de seriële verbinding
 serial_connection.close()
