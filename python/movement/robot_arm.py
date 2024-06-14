0ef6f8c0123bf7f321dfcc04d49b15a7c4dab12d
joelsefanja <joelsefanja@gmail.com>
2024-06-14 13:36:07 +0200

    Serial connection fixed?

--- python/movement/robot_arm.py
+++ python/movement/robot_arm.py
@@ -18,25 +18,25 @@ class RobotArm:
                 time.sleep(0.1)  # Korte vertraging toevoegen
                 serial_connection.goto(SERVO_2, elbow_angle, speed=20, degrees=True)
                 time.sleep(0.1)  # Korte vertraging toevoegen
-                print(self.serial_connection.ping(SERVO_1))
-                print(self.serial_connection.ping(SERVO_2))
+                print(serial_connection.ping(SERVO_1))
+                print(serial_connection.ping(SERVO_2))
                 print(f"Moving to position: Shoulder angle: {shoulder_angle}, Elbow angle: {elbow_angle}")
             except Exception as e:
                 print(f"Error moving to position: {e}")
 
     def get_angles_from_arm(self):
-        if not self.serial_connection:
+        if not serial_connection:
             print("Serial connection not established.")
             return None, None
         try:
-            shoulder_angle = self.serial_connection.get_present_position(SERVO_1, degrees=True)
-            elbow_angle = self.serial_connection.get_present_position(SERVO_2, degrees=True)
+            shoulder_angle = serial_connection.get_present_position(SERVO_1, degrees=True)
+            elbow_angle = serial_connection.get_present_position(SERVO_2, degrees=True)
             return shoulder_angle, elbow_angle
         except Exception as e:
             print(f"Error getting angles: {e}")
             return None, None
 
     def close_connection(self):
-        if self.serial_connection:
-            self.serial_connection.close()
+        if serial_connection:
+            serial_connection.close()
             print("Serial connection closed.")
