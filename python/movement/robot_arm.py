fed3f27bdbc932371e7f5e22f3f063b1c1364952
joelsefanja <joelsefanja@gmail.com>
2024-06-14 13:31:39 +0200

    Add vertraging

--- python/movement/robot_arm.py
+++ python/movement/robot_arm.py
@@ -14,17 +14,19 @@ class RobotArm:
             self.serial_connection = None
 
     def move_to_position(self, shoulder_angle, elbow_angle):
-        if not self.serial_connection:
-            print("Serial connection not established.")
-            return
-        try:
-            self.serial_connection.goto(SERVO_1, shoulder_angle, speed=20, degrees=True)
-            self.serial_connection.goto(SERVO_2, elbow_angle, speed=20, degrees=True)
-            print(self.serial_connection.ping(SERVO_1))
-            print(self.serial_connection.ping(SERVO_2))
-            print(f"Moving to position: Shoulder angle: {shoulder_angle}, Elbow angle: {elbow_angle}")
-        except Exception as e:
-            print(f"Error moving to position: {e}")
+            if not self.serial_connection:
+                print("Serial connection not established.")
+                return
+            try:
+                self.serial_connection.goto(SERVO_1, shoulder_angle, speed=20, degrees=True)
+                time.sleep(0.1)  # Korte vertraging toevoegen
+                self.serial_connection.goto(SERVO_2, elbow_angle, speed=20, degrees=True)
+                time.sleep(0.1)  # Korte vertraging toevoegen
+                print(self.serial_connection.ping(SERVO_1))
+                print(self.serial_connection.ping(SERVO_2))
+                print(f"Moving to position: Shoulder angle: {shoulder_angle}, Elbow angle: {elbow_angle}")
+            except Exception as e:
+                print(f"Error moving to position: {e}")
 
     def get_angles_from_arm(self):
         if not self.serial_connection:
