6f7fa63ccd1da43d8c69a2b092b76a02b8d77ba6
JoostBakker28 <113503680+JoostBakker28@users.noreply.github.com>
2024-06-19 14:18:12 +0200

    Update coordinate_transformation.py

--- python/camera/coordinate_transformation.py
+++ python/camera/coordinate_transformation.py
@@ -11,7 +11,7 @@ class CoordinateTransformer:
         self.convertion_rate = convertion_rate
         
         # Load camera calibration data
-        sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+        sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
         calibration_data = np.load('calibration_data.npz')
         self.mtx = calibration_data['mtx']
         self.dist = calibration_data['dist']
